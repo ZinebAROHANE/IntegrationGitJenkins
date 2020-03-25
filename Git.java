ea15a10487551639fc7af8d5ad1b55305bf90825
ZinebAROHANE <61824624+ZinebAROHANE@users.noreply.github.com>
2020-03-25 21:41:30 +0000

    Update Git.java

--- Git.java
+++ Git.java
@@ -3,6 +3,7 @@ public class Git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test de push git!! ");
+		System.out.println("Version 1.1 ");
 	}
 
 }
